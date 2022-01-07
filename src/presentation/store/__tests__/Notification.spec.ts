19cd1958b3f6779bd07bafa53e90a651b6bed223
avrcoelho <andrevrcoelho@hotmail.com>
2022-01-06 21:37:01 -0300

    test: notifications store

--- src/presentation/store/__tests__/Notification.spec.ts
+++ src/presentation/store/__tests__/Notification.spec.ts
@@ -1,3 +1,22 @@
 import { NotificationStore } from '../NotificationStore';
 
-describe;
+describe('NotificationStore', () => {
+  it('should be able to add notification', () => {
+    const notificationStore = NotificationStore.getInstance();
+    notificationStore.subscribe(jest.fn());
+
+    notificationStore.add('test');
+
+    expect(notificationStore.get()).toEqual(['test']);
+  });
+
+  it('should be able to remove notification', () => {
+    const notificationStore = NotificationStore.getInstance();
+    notificationStore.subscribe(jest.fn());
+
+    notificationStore.add('test');
+    notificationStore.remove('test');
+
+    expect(notificationStore.get()).toEqual([]);
+  });
+});
