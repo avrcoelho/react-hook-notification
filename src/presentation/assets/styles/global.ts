45a756743c0bfaeb0e9909ce2423a1fcc2ea9f75
avrcoelho <andrevrcoelho@hotmail.com>
2022-01-10 21:33:25 -0300

    fix: eslint issues

--- src/presentation/assets/styles/global.ts
+++ src/presentation/assets/styles/global.ts
@@ -1,13 +1,13 @@
-import { globalCss } from "@stitches/react";
-import "./inter.css";
+import { globalCss } from '@stitches/react';
+import './inter.css';
 
 export const globalStyles = globalCss({
-  ":root": {
-    fontSize: "10px",
+  ':root': {
+    fontSize: '10px',
   },
-  "*": { margin: 0, padding: 0, boxSizing: "border-box", outline: 0 },
+  '*': { margin: 0, padding: 0, boxSizing: 'border-box', outline: 0 },
   body: {
     fontFamily: "'Inter', sans-serif",
-    fontSize: "1.6rem",
+    fontSize: '1.6rem',
   },
 });
