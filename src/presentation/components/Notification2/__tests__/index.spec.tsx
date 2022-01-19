b3892fa4585d69b52199b200d9dc78479e66e955
avrcoelho <andrevrcoelho@hotmail.com>
2022-01-19 12:25:40 -0300

    chore: element not used

--- src/presentation/components/Notification2/__tests__/index.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import { Notification } from '..';
-
-it('test', () => {
-  expect(() => {
-    render(<Notification />);
-  }).not.toThrow();
-});
