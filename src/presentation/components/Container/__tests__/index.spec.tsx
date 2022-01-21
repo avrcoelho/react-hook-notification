95e1185b48b9b36be3a4bb041518bec97c708d9d
avrcoelho <andrevrcoelho@hotmail.com>
2022-01-21 11:49:32 -0300

    test: container

---
+++ src/presentation/components/Container/__tests__/index.spec.tsx
@@ -0,0 +1,39 @@
+import { screen, render } from '@testing-library/react';
+
+import { NotificationPositions } from '@/presentation/constants/NotificationPositions';
+import { NotificationTypes } from '@/presentation/constants/NotificationTypes';
+import { Container } from '..';
+
+let mockShow = true;
+jest.mock('../useController', () => ({
+  useController: () => ({
+    show: mockShow,
+  }),
+}));
+
+describe('Container component', () => {
+  const props = {
+    notifications: [
+      {
+        id: 'id-test',
+        type: NotificationTypes.Error,
+        text: 'text test',
+      },
+    ],
+    isVisible: true,
+    onRemove: jest.fn(),
+    position: NotificationPositions.TopRight,
+  };
+  it('should be able to render notification', () => {
+    render(<Container {...props} />);
+
+    expect(screen.getByText(props.notifications[0].text)).toBeTruthy();
+  });
+
+  it('should not be able to render notification', () => {
+    mockShow = false;
+    render(<Container {...props} />);
+
+    expect(screen.queryByText(props.notifications[0].text)).toBeFalsy();
+  });
+});
