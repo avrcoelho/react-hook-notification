8e92a998bbcd870d591aec322cccb9692df8d6b5
avrcoelho <andrevrcoelho@hotmail.com>
2022-01-24 13:27:16 -0300

    chore: exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export { useNotification } from './main';
+import { useNotification } from './main';
+import {
+  NotificationPosition,
+  NotificationTheme,
+  NotificationType,
+  NotificationTransition,
+} from './presentation/types/Notification';
+
+export {
+  NotificationPosition,
+  NotificationTheme,
+  NotificationType,
+  NotificationTransition,
+  useNotification,
+};
