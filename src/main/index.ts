cc075a8194eb1edfe51aef4340f4e86ef3e365e9
avrcoelho <andrevrcoelho@hotmail.com>
2022-03-08 22:47:43 -0300

    fix: memoization

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -21,84 +22,83 @@ const notificationStore = NotificationStore.getInstance();
 const initialize = Initialize.getInstance();
 initialize.render(Notifications);
 
-export const useNotification: UseNotificationHook = (
-  hookParams = { position: 'top-right' },
-) => {
-  const info = useCallback(
-    (notificationParams: NotificationParams) => {
-      const id = uuidv4();
-      const notification = {
-        ...hookParams,
-        ...notificationParams,
-        id,
-        type: 'info' as NotificationType,
-      };
-      notificationStore.add(notification);
-    },
-    [hookParams],
-  );
+export const useNotification: UseNotificationHook = () =>
+  useCallback((hookParams = { position: 'top-right' }) => {
+    const info = useCallback(
+      (notificationParams: NotificationParams) => {
+        const id = uuidv4();
+        const notification = {
+          ...hookParams,
+          ...notificationParams,
+          id,
+          type: 'info' as NotificationType,
+        };
+        notificationStore.add(notification);
+      },
+      [hookParams],
+    );
 
-  const success = useCallback(
-    (notificationParams: NotificationParams) => {
-      const id = uuidv4();
-      const notification = {
-        ...hookParams,
-        ...notificationParams,
-        id,
-        type: 'success' as NotificationType,
-      };
-      notificationStore.add(notification);
-    },
-    [hookParams],
-  );
+    const success = useCallback(
+      (notificationParams: NotificationParams) => {
+        const id = uuidv4();
+        const notification = {
+          ...hookParams,
+          ...notificationParams,
+          id,
+          type: 'success' as NotificationType,
+        };
+        notificationStore.add(notification);
+      },
+      [hookParams],
+    );
 
-  const error = useCallback(
-    (notificationParams: NotificationParams) => {
-      const id = uuidv4();
-      const notification = {
-        ...hookParams,
-        ...notificationParams,
-        id,
-        type: 'error' as NotificationType,
-      };
-      notificationStore.add(notification);
-    },
-    [hookParams],
-  );
+    const error = useCallback(
+      (notificationParams: NotificationParams) => {
+        const id = uuidv4();
+        const notification = {
+          ...hookParams,
+          ...notificationParams,
+          id,
+          type: 'error' as NotificationType,
+        };
+        notificationStore.add(notification);
+      },
+      [hookParams],
+    );
 
-  const warning = useCallback(
-    (notificationParams: NotificationParams) => {
-      const id = uuidv4();
-      const notification = {
-        ...hookParams,
-        ...notificationParams,
-        id,
-        type: 'warning' as NotificationType,
-      };
-      notificationStore.add(notification);
-    },
-    [hookParams],
-  );
+    const warning = useCallback(
+      (notificationParams: NotificationParams) => {
+        const id = uuidv4();
+        const notification = {
+          ...hookParams,
+          ...notificationParams,
+          id,
+          type: 'warning' as NotificationType,
+        };
+        notificationStore.add(notification);
+      },
+      [hookParams],
+    );
 
-  const defaultNotification = useCallback(
-    (notificationParams: NotificationParams) => {
-      const id = uuidv4();
-      const notification = {
-        ...hookParams,
-        ...notificationParams,
-        id,
-        type: 'default' as NotificationType,
-      };
-      notificationStore.add(notification);
-    },
-    [hookParams],
-  );
+    const defaultNotification = useCallback(
+      (notificationParams: NotificationParams) => {
+        const id = uuidv4();
+        const notification = {
+          ...hookParams,
+          ...notificationParams,
+          id,
+          type: 'default' as NotificationType,
+        };
+        notificationStore.add(notification);
+      },
+      [hookParams],
+    );
 
-  return {
-    info,
-    error,
-    success,
-    warning,
-    default: defaultNotification,
-  };
-};
+    return {
+      info,
+      error,
+      success,
+      warning,
+      default: defaultNotification,
+    };
+  }, [])();
