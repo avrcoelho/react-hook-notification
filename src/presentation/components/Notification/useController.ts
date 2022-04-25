14bd89f533fcef7a470b704458a83f6c7ad689dd
avrcoelho <andrevrcoelho@hotmail.com>
2022-04-25 19:11:23 -0300

    chore: removed unused import

--- src/presentation/components/Notification/useController.ts
+++ src/presentation/components/Notification/useController.ts
@@ -1,10 +1,4 @@
-import {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   MotionValue,
   PanInfo,
